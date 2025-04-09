88f93682df2ab055c08b56e5cbf681ceccaa9e6d
kimtaeuk1995 <67112686+kimtaeuk1995@users.noreply.github.com>
2025-04-08 21:35:43 -0400

    initial

---
+++ frontend/app/header.js
@@ -0,0 +1,12 @@
+"use client";
+
+import '../globals.css';
+
+function Header() {
+    return (
+        <header style={{ textAlign: 'center' }}>
+            <h1 style={{ fontSize: '24px' }}>Diabetes - Self Management Tool</h1>
+        </header>
+    );
+}
+export default Header;
