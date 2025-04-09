82f0b9046e0e7ca080bd4023d991a86c75671aea
kimtaeuk1995 <67112686+kimtaeuk1995@users.noreply.github.com>
2025-04-08 22:54:59 -0400

    initial

--- frontend/app/header.tsx
+++ frontend/app/header.tsx
@@ -3,9 +3,6 @@
 export default function Header() {
   return (
     <header style={{ textAlign: 'center', marginTop: '1rem' }}>
-      <h1 style={{ fontSize: '24px', fontWeight: 'bold' }}>
-        Diabetes - Self Management Tool
-      </h1>
     </header>
   );
 }
